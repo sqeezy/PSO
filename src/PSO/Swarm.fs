e511aa2631f6420162c29406d67c5b47bb352a29
Anton Herzog <info@antonherzog.com>
2018-11-06 07:16:30 +0100

    Cleanup of swarm logic.

--- src/PSO/Swarm.fs
+++ src/PSO/Swarm.fs
@@ -5,18 +5,16 @@ open System
 module Swarm =
 
   //from paper https://hal.archives-ouvertes.fr/file/index/docid/764996/filename/SPSO_descriptions.pdf
-  let typicalSwarmSize dimension = 10 + 2 * (dimension |> float |> Math.Sqrt |> int)
+  let private typicalSwarmSize dimension = 10 + 2 * (dimension |> float |> Math.Sqrt |> int)
+  let private valueFromSolution solution =
+    let (_ , value) = solution
+    value
+  let private bestParticle particles = particles |> Seq.minBy (fun p -> p.LocalBest |> valueFromSolution)
 
   let create problem =
     let particles = [1 .. (typicalSwarmSize problem.Dimension)] 
                       |> List.map (fun _ -> Particle.create problem)
-
-    let valueFromSolution solution =
-      let (_ , value) = solution
-      value
-
-    let initialBestParticle = particles |> Seq.minBy (fun p -> p.LocalBest |> valueFromSolution)
-    
+    let initialBestParticle = bestParticle particles
     {
       GlobalBest = initialBestParticle.LocalBest;
       Particles = particles
