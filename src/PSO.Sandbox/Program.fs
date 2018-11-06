cc6e3cf96879397dceb7571c9a3df369b1893ccd
Anton Herzog <info@antonherzog.com>
2018-11-06 06:58:07 +0100

    Cleanup in test project. Fixed test functions.

--- src/PSO.Sandbox/Program.fs
+++ src/PSO.Sandbox/Program.fs
@@ -7,59 +7,52 @@ let oneDimPolynoma =
     let x = Seq.item 0 parameters
     // f(x) = x³ - 6x² + 4X + 12
     (x*x*x - 6.*x*x + 4.*x + 12.)
-
-  let sum (parameters:ParameterSet):Fitnesse = 
-    Seq.sum parameters
-
-  let problem = {
+  {
     Func = xSquared;
     InputRange = (-1., 10.);
     MaxVelocity=0.1;
     Dimension = 1
   }
-  problem
 
 let xSquaredYSquaredProblem =
   let xSquaredAndYSquared (parameters:ParameterSet):Fitnesse = 
     let p1 = Seq.item 0 parameters
-    let p2 = Seq.item 0 parameters
+    let p2 = Seq.item 1 parameters
     p1*p1+p2*p2
-
-  let sum (parameters:ParameterSet):Fitnesse = 
-    Seq.sum parameters
-
-  let problem = {
+  {
     Func = xSquaredAndYSquared;
     InputRange = (-5., 5.);
     MaxVelocity=0.1;
     Dimension = 2
   }
-  problem
-
 
 let xSquaredProblem =
   let xSquared (parameters:ParameterSet):Fitnesse = 
     let p1 = Seq.item 0 parameters
     p1*p1
-
-  let sum (parameters:ParameterSet):Fitnesse = 
-    Seq.sum parameters
-
-  let problem = {
+  {
     Func = xSquared;
     InputRange = (0., 5.);
     MaxVelocity=0.1;
     Dimension = 1
   }
-  problem
+
+let xSquaredYSquaredZSquaredProblem =
+  let xxyyzz (parameters:ParameterSet):Fitnesse = 
+    let p1 = Seq.item 0 parameters
+    let p2 = Seq.item 1 parameters
+    let p3 = Seq.item 2 parameters
+    p1*p1+p2*p2+p3*p3
+  {
+    Func = xxyyzz;
+    InputRange = (-5., 5.);
+    MaxVelocity=0.1;
+    Dimension = 3
+  }
 
 [<EntryPoint>]
 let Main argv = 
-    
     let config = {MaxIterations=1000}
-    
-    let solution = SequentialOptimizer.create oneDimPolynoma config log
-
+    let solution = SequentialOptimizer.create xSquaredYSquaredZSquaredProblem config log
     printfn "%A" solution
-    
     0
